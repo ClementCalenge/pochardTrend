6260377dd97c25a46f5d517df932a3ea7dc39368
Clement Calenge <clement.calenge@gmail.com>
2022-02-05 16:25:38 +0100

    update the vignette
    Explain why longitude is also included in the dataset, though
    not used in the model

--- inst/doc/pochardTrend.R
+++ inst/doc/pochardTrend.R
@@ -1,4 +1,4 @@
-## ----setup, include=FALSE, cache=FALSE----------------------------------------
+## ----setup, include=FALSE, cache=FALSE--------------------
 # set global chunk options
 library('knitr')
 opts_chunk$set(fig.path="caperpy-",
@@ -21,7 +21,7 @@ options(replace.assign=TRUE,width=60)
 set.seed(9567)
 
 
-## ----eval=FALSE---------------------------------------------------------------
+## ----eval=FALSE-------------------------------------------
 ## ## If devtools is not yet installed, type
 ## install.packages("devtools")
 ## 
@@ -29,20 +29,20 @@ set.seed(9567)
 ## devtools::install_github("ClementCalenge/pochardTrend", ref="main")
 
 
-## -----------------------------------------------------------------------------
+## ---------------------------------------------------------
 library(pochardTrend)
 
 
-## -----------------------------------------------------------------------------
+## ---------------------------------------------------------
 head(pochard)
 
 
-## -----------------------------------------------------------------------------
+## ---------------------------------------------------------
 plot(euromap, col="grey")
 plot(quadratMap, col="yellow", add=TRUE)
 
 
-## ----eval=FALSE---------------------------------------------------------------
+## ----eval=FALSE-------------------------------------------
 ## fittedModel <- fitModel(pochard$count, pochard$site, pochard$year,
 ##                         pochard$idg, pochard$latq)
 
@@ -53,18 +53,18 @@ qqnorm(fittedModel$random$bqi[,2], main="Random effect grid cell b_q(i)")
 qqnorm(fittedModel$random$di[,2], main="Random effect site d_i")
 
 
-## -----------------------------------------------------------------------------
+## ---------------------------------------------------------
 qqnorm(fittedModel$random$eit)
 
 
-## -----------------------------------------------------------------------------
+## ---------------------------------------------------------
 library(ggplot2)
 dae <- data.frame(year=pochard$year, eit=fittedModel$random$eit)
 ggplot(dae,aes(x=factor(year), y=eit))+geom_violin()+
     geom_boxplot(width=0.2, fill="grey")
 
 
-## -----------------------------------------------------------------------------
+## ---------------------------------------------------------
 ## Coordinates of the centroids of the grid cells 
 xy <- st_coordinates(st_centroid(quadratMap))
 
@@ -77,23 +77,23 @@ gnb[[185]] <- 184L
 gnb[[184]] <- 185L
 
 
-## -----------------------------------------------------------------------------
+## ---------------------------------------------------------
 ## Moran test
 moran.test(fittedModel$random$bqi[,2], nb2listw(gnb))
 
 
-## -----------------------------------------------------------------------------
+## ---------------------------------------------------------
 fittedModel
 
 
-## -----------------------------------------------------------------------------
+## ---------------------------------------------------------
 beta0 <- fittedModel$fixed$beta0
 latitude_effect <- fittedModel$fixed$latitude_effect
 
 beta0 + latitude_effect*c(46,60)
 
 
-## -----------------------------------------------------------------------------
+## ---------------------------------------------------------
 ## We load MASS to simulate a multivariate
 library(MASS)
 
@@ -122,7 +122,7 @@ sd(dimpa60)
 sd(dimpa46)
 
 
-## -----------------------------------------------------------------------------
+## ---------------------------------------------------------
 dimpe60 <- (1-exp(apply(simv,1,function(x) 11*(x[2]*60+x[1]))))
 dimpe46 <- (1-exp(apply(simv,1,function(x) 11*(x[2]*46+x[1]))))
 
@@ -135,7 +135,7 @@ sd(dimpe60)
 sd(dimpe46)
 
 
-## -----------------------------------------------------------------------------
+## ---------------------------------------------------------
 ## Point estimate
 1-exp(beta0+latitude_effect*mean(pochard$latq))
 
@@ -144,7 +144,7 @@ dimens <- (1-exp(apply(simv,1,function(x) x[2]*mean(pochard$latq)+x[1])))
 sd(dimens)
 
 
-## -----------------------------------------------------------------------------
+## ---------------------------------------------------------
 ## Point estimate
 1-exp(11*(beta0+latitude_effect*mean(pochard$latq)))
 
@@ -153,18 +153,18 @@ dimenso <- (1-exp(apply(simv,1,function(x) 11*(x[2]*mean(pochard$latq)+x[1]))))
 sd(dimenso)
 
 
-## -----------------------------------------------------------------------------
+## ---------------------------------------------------------
 lat <- seq(45, 60, by=5)
 lat[1] <- 46
 
 showTrends(fittedModel, 2002:2012,lat)+theme(legend.position = "none")
 
 
-## -----------------------------------------------------------------------------
+## ---------------------------------------------------------
 fittedModel
 
 
-## -----------------------------------------------------------------------------
+## ---------------------------------------------------------
 ## sigma_bqi
 ## point estimate
 mean(exp(rnorm(100000, fittedModel$rawTMB$par.fixed["log_sigmabqi"],
